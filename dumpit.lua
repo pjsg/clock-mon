dd9dd39cc2c2adbbec910ccf3e42605238d2c009
philip <philip@gladstonefamily.net>
2020-08-29 17:54:19 -0400

    Dump the last 20k of the flash chip

--- dumpit.lua
+++ dumpit.lua
@@ -1,7 +1,7 @@
 function dumpit()
   local hasher = crypto.new_hash('sha1')
-  local offset = 0x3fd000
-  for addr = 0, 0x2fff, 128 do
+  local offset = 0x3fb000
+  for addr = 0, 0x4fff, 128 do
     hasher:update(node.peek(addr + offset, 128))
   end
   local hash = string.sub(encoder.toHex(hasher:finalize()), 1, 16)
@@ -12,7 +12,7 @@ function dumpit()
     num = sjson.decode(current).num
   end
   local d = file.open(string.format("flash-%d-%s", num, hash), "w")
-  for addr = 0, 0x2fff, 128 do
+  for addr = 0, 0x4fff, 128 do
     d:write(node.peek(addr + offset, 128))
   end
   d:close()
